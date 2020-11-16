c305a8036e6273daf1162065777aa68d2df557f8
Lexie Hansen <home@Lexies-MacBook-Pro.local>
2020-11-15 19:51:03 -0700

    add for loop to generate password, correct spelling errors, reconfigure if else statements

--- script.js
+++ script.js
@@ -10,54 +10,65 @@ var confirmSpecialCharacters;
 
 //arrays
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
-
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
-
 
 //generate function
 function generatePassword() {
+
   //password length pop up
   var passwordLength = (prompt("How long would you like your password to be? Enter a numerical value between 8 and 128."));
-    if (passwordLength <= 7 || passwordLength >= 129) {
-      window.alert("Please enter a number between 8 and 128.")
+    
+  if (passwordLength <= 7 || passwordLength >= 129) {
+      alert("You must choose a value between 8 and 128. Please try again.")
     }
-
-  //variables pop ups
-  confirmUppercase = confirm("Click OK if you would like to use uppercase letters.")
-  confirmLowercase = confirm("Click OK if you would like to use lowercase letters.")
-  confirmNumbers = confirm("Click OK if you would like to use numbers.")
-  confirmSpecialCharacters = confirm("Click OK if you would like to use special characters.")
-    //checking for pop up answer
-    while(lowerCase === false && upperCase === false && numbers === false && special === false){
-      alert("Please choose at least one character type.")
+    
+    else {
+      //choose info pop ups
       confirmUppercase = confirm("Click OK if you would like to use uppercase letters.")
       confirmLowercase = confirm("Click OK if you would like to use lowercase letters.")
       confirmNumbers = confirm("Click OK if you would like to use numbers.")
       confirmSpecialCharacters = confirm("Click OK if you would like to use special characters.")
+        
+        //checking for pop up answer
+        while(confirmUppercase === false && confirmLowercase === false && confirmNumbers === false && confirmSpecialCharacters === false){
+          alert("Please choose at least one character type.")
+          confirmUppercase = confirm("Click OK if you would like to use uppercase letters.")
+          confirmLowercase = confirm("Click OK if you would like to use lowercase letters.")
+          confirmNumbers = confirm("Click OK if you would like to use numbers.")
+          confirmSpecialCharacters = confirm("Click OK if you would like to use special characters.")
+        }
     }
 
     //generating password info
     var passwordInfo = []
-     if (confirmUppercase === true) {
+
+     if (confirmUppercase) {
        passwordInfo = passwordInfo.concat(uppercase)
      }
-     if (confirmLowercase === true) {
+
+     if (confirmLowercase) {
        passwordInfo = passwordInfo.concat(lowercase)
      }
-     if (confirmNumbers === true) {
+
+     if (confirmNumbers) {
        passwordInfo = passwordInfo.concat(numbers)
      }
-     if (confirmSpecialCharacters === true) {
+
+     if (confirmSpecialCharacters) {
        passwordInfo = passwordInfo.concat(specialCharacters)
      }
 
+     //generating password
      var generatedPassword = ""
 
+     for (i=0; i<passwordLength; i++) {
+      generatedPassword = generatedPassword + passwordInfo[Math.floor(Math.random() * passwordInfo.length)]
+    }
+    return generatedPassword;
 };
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
